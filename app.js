799111f7e7c54bc4b54a7a5e122fb4a5d6441245
dvideby0 <rbrookfield@352inc.com>
2015-06-28 18:47:40 -0400

    initial commit

--- app.js
+++ app.js
@@ -12,19 +12,21 @@ app.use('/', express.static('public'));
 app.post('/', function(req, res){
   console.log(req.body) // form fields
   console.log(req.files.filefield.path) // form files
-  var file = req.files.filefield.path;
+  if(req.files) {
+    var file = req.files.filefield.path;
 
-  var filename = path.basename(file);
-  var mimetype = mime.lookup(file);
+    var filename = path.basename(file);
+    var mimetype = mime.lookup(file);
 
-  child = exec('composite -gravity center flag.jpg[390x^] -geometry +90-15 ' + file + '[390x] -dissolve 30 ' + file,
-    function (error, stdout, stderr) {
-      res.setHeader('Content-disposition', 'attachment; filename=' + filename);
-      res.setHeader('Content-type', mimetype);
+    child = exec('composite -gravity center flag.jpg[390x^] -geometry +90-15 ' + file + '[390x] -dissolve 30 ' + file,
+      function (error, stdout, stderr) {
+        res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+        res.setHeader('Content-type', mimetype);
 
-      var filestream = fs.createReadStream(file);
-      filestream.pipe(res);
-  });
+        var filestream = fs.createReadStream(file);
+        filestream.pipe(res);
+    });
+  }
 });
 
 app.listen(process.env.PORT || 8000);
