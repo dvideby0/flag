619e60e943b8c47092d08abee356c0eb6bdf000f
dvideby0 <rbrookfield@352inc.com>
2015-07-03 09:26:27 -0400

    removing console logs

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ var app = express();
 var request = require('request');
 var download = function(uri, filename, callback){
   request.head(uri, function(err, res, body){
-    console.log('content-type:', res.headers['content-type']);
-    console.log('content-length:', res.headers['content-length']);
-
     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
   });
 };
